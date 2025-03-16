4d43f4c14640c858c624c094bf5d106912bbcfc1
Duude92 <switch4acc@gmail.com>
2025-03-16 10:28:35 +0200

    Refactor main function

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"github.com/caarlos0/env/v11"
 	"log"
 	"os"
@@ -23,36 +24,56 @@ type DbConfig struct {
 	DBName     string `env:"DB_NAME"`
 }
 
-func main() {
-	tgApiKey := os.Getenv("ApiKey")
-	//dbHost, dbPort, dbUser, dbPassword, dbName := os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWD"), os.Getenv("DB_NAME")
+func loadConfig() (*DbConfig, error) {
 	var config DbConfig
 	err := env.Parse(&config)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
+	return &config, nil
+}
+
+func main() {
+	if err := initDb(); err != nil {
+		log.Fatalf("Unable to initialize database %v", err)
+	}
+
+	if err := initTgBot(); err != nil {
+		log.Fatalf("Unable to initialize Telegram bot %v", err)
+	}
+}
+
+func initTgBot() error {
+	tgApiKey := os.Getenv("ApiKey")
 	if tgApiKey == "" {
-		log.Fatal("Telegram Api key is not set")
+		return errors.New("Telegram Api key is not set")
 	}
 	tgApiKey = strings.TrimSpace(tgApiKey)
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer cancel()
-
-	Models.CreateDb(config.Host, config.DBPort, config.DBUser, config.DBPassword, config.DBName)
-
 	opts := []bot.Option{
 		bot.WithDefaultHandler(handler),
 	}
-
 	b, err := bot.New(tgApiKey, opts...)
 	if err != nil {
-		panic(err)
+		return err
 	}
-	b.RegisterHandler(bot.HandlerTypeMessageText, "/start", bot.MatchTypeExact, myStartHandler)
-	b.RegisterHandler(bot.HandlerTypeMessageText, "/stop", bot.MatchTypeExact, myStopHandler)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/start", bot.MatchTypeExact, startHandler)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/stop", bot.MatchTypeExact, stopHandler)
 	b.RegisterHandler(bot.HandlerTypeMessageText, "/delete", bot.MatchTypeExact, deleteHandler)
 
 	b.Start(ctx)
+	return nil
+}
+
+func initDb() error {
+	config, err := loadConfig()
+	if err != nil {
+		return err
+	}
+	return Models.CreateDb(config.Host, config.DBPort, config.DBUser, config.DBPassword, config.DBName)
 }
 
 func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
@@ -62,25 +83,34 @@ func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
 	})
 }
 func deleteHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
-	Models.DeleteUser(update.Message.Chat.ID)
+	err := Models.DeleteUser(update.Message.Chat.ID)
+	if err != nil {
+		log.Fatalf("Unable to delete user %v", err)
+	}
 }
-func myStartHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
-	Models.AddUser(&Models.User{
+func startHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	err := Models.AddUser(&Models.User{
 		Id:     update.Message.Chat.ID,
 		Name:   update.Message.Chat.Username,
 		Status: true,
 	})
+	if err != nil {
+		log.Fatalf("Unable to add user %v", err)
+	}
 	b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: update.Message.Chat.ID,
 		Text:   `User registered as ` + update.Message.Chat.Username,
 	})
 }
-func myStopHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
-	Models.UpdateUser(&Models.User{
+func stopHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	err := Models.UpdateUser(&Models.User{
 		Id:     update.Message.Chat.ID,
 		Name:   update.Message.Chat.Username,
 		Status: false,
 	})
+	if err != nil {
+		log.Fatalf("Unable to disable user %v", err)
+	}
 	b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: update.Message.Chat.ID,
 		Text:   `User unregistered as ` + update.Message.Chat.Username,
